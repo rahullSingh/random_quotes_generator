640197262374b2645d72bb8506604b3ad49cf18a
rahul kumar s <rahu143rspt3gailcom>
2021-11-08 12:17:21 +0530

    quotes

---
+++ index.js
@@ -0,0 +1,21 @@
+const thought=document.querySelector(".thought");
+const author=document.querySelector(".author");
+
+document.querySelector("button").addEventListener("click",newThoughtGenerator)
+document.querySelector("button").style.backgroundColor="#92D8C0"
+
+newThoughtGenerator();
+
+async function newThoughtGenerator(){
+  const response= await fetch("https://type.fit/api/quotes");
+  const data= await response.json();
+  const quote=data[Math.floor(Math.random()*data.length)]
+
+  thought.innerText=quote.text;
+  author.innerText=quote.author;
+
+  thought.style.color="#B5B5B5";
+  // author.style.color="#8E8E8E";
+  author.style.color="#000000";
+
+}
